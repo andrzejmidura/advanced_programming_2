71ced3fd4549c26501ebef49edac2dce84f23678
andrzejmidura <andrzej.midura253@gmail.com>
2022-04-08 19:38:30 +0200

    implementation

--- CarRental/src/OrderForm.Designer.cs
+++ CarRental/src/OrderForm.Designer.cs
@@ -29,12 +29,200 @@ namespace CarRental
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cancelOrderButton = new System.Windows.Forms.Button();
+            this.returnButton = new System.Windows.Forms.Button();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.surnameLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.tableLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.ordersDataGrid = new System.Windows.Forms.DataGridView();
+            this.showOrdersButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cancelOrderButton
+            // 
+            this.cancelOrderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cancelOrderButton.Location = new System.Drawing.Point(646, 381);
+            this.cancelOrderButton.Name = "cancelOrderButton";
+            this.cancelOrderButton.Size = new System.Drawing.Size(137, 30);
+            this.cancelOrderButton.TabIndex = 0;
+            this.cancelOrderButton.Text = "Cancel order";
+            this.cancelOrderButton.UseVisualStyleBackColor = true;
+            this.cancelOrderButton.Click += new System.EventHandler(this.cancelOrderButton_Click);
+            // 
+            // returnButton
+            // 
+            this.returnButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.returnButton.Location = new System.Drawing.Point(217, 381);
+            this.returnButton.Name = "returnButton";
+            this.returnButton.Size = new System.Drawing.Size(137, 30);
+            this.returnButton.TabIndex = 1;
+            this.returnButton.Text = "Return to menu";
+            this.returnButton.UseVisualStyleBackColor = true;
+            this.returnButton.Click += new System.EventHandler(this.returnButton_Click);
+            // 
+            // orderButton
+            // 
+            this.orderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.orderButton.Location = new System.Drawing.Point(360, 381);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(137, 30);
+            this.orderButton.TabIndex = 2;
+            this.orderButton.Text = "Order";
+            this.orderButton.UseVisualStyleBackColor = true;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameLabel.Location = new System.Drawing.Point(56, 119);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(55, 20);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name:";
+            // 
+            // surnameLabel
+            // 
+            this.surnameLabel.AutoSize = true;
+            this.surnameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.surnameLabel.Location = new System.Drawing.Point(33, 160);
+            this.surnameLabel.Name = "surnameLabel";
+            this.surnameLabel.Size = new System.Drawing.Size(78, 20);
+            this.surnameLabel.TabIndex = 4;
+            this.surnameLabel.Text = "Surname:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addressLabel.Location = new System.Drawing.Point(39, 202);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(72, 20);
+            this.addressLabel.TabIndex = 5;
+            this.addressLabel.Text = "Address:";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.phoneLabel.Location = new System.Drawing.Point(52, 243);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(59, 20);
+            this.phoneLabel.TabIndex = 6;
+            this.phoneLabel.Text = "Phone:";
+            // 
+            // tableLabel
+            // 
+            this.tableLabel.AutoSize = true;
+            this.tableLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tableLabel.Location = new System.Drawing.Point(623, 37);
+            this.tableLabel.Name = "tableLabel";
+            this.tableLabel.Size = new System.Drawing.Size(210, 20);
+            this.tableLabel.TabIndex = 7;
+            this.tableLabel.Text = "Already ordered vehicles:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameTextBox.Location = new System.Drawing.Point(130, 116);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(260, 26);
+            this.nameTextBox.TabIndex = 9;
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.surnameTextBox.Location = new System.Drawing.Point(130, 157);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(260, 26);
+            this.surnameTextBox.TabIndex = 10;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addressTextBox.Location = new System.Drawing.Point(130, 201);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(260, 26);
+            this.addressTextBox.TabIndex = 11;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.phoneTextBox.Location = new System.Drawing.Point(130, 242);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(260, 26);
+            this.phoneTextBox.TabIndex = 12;
+            // 
+            // ordersDataGrid
+            // 
+            this.ordersDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ordersDataGrid.Location = new System.Drawing.Point(443, 77);
+            this.ordersDataGrid.Name = "ordersDataGrid";
+            this.ordersDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ordersDataGrid.Size = new System.Drawing.Size(552, 258);
+            this.ordersDataGrid.TabIndex = 13;
+            // 
+            // showOrdersButton
+            // 
+            this.showOrdersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.showOrdersButton.Location = new System.Drawing.Point(503, 381);
+            this.showOrdersButton.Name = "showOrdersButton";
+            this.showOrdersButton.Size = new System.Drawing.Size(137, 30);
+            this.showOrdersButton.TabIndex = 14;
+            this.showOrdersButton.Text = "Show my orders";
+            this.showOrdersButton.UseVisualStyleBackColor = true;
+            this.showOrdersButton.Click += new System.EventHandler(this.showOrdersButton_Click);
+            // 
+            // OrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1018, 450);
+            this.Controls.Add(this.showOrdersButton);
+            this.Controls.Add(this.ordersDataGrid);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.tableLabel);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.surnameLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.orderButton);
+            this.Controls.Add(this.returnButton);
+            this.Controls.Add(this.cancelOrderButton);
+            this.Name = "OrderForm";
             this.Text = "Order";
+            ((System.ComponentModel.ISupportInitialize)(this.ordersDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cancelOrderButton;
+        private System.Windows.Forms.Button returnButton;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label surnameLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label tableLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.DataGridView ordersDataGrid;
+        private System.Windows.Forms.Button showOrdersButton;
     }
 }
--- CarRental/src/OrderForm.cs
+++ CarRental/src/OrderForm.cs
@@ -12,9 +12,101 @@ namespace CarRental
 {
     public partial class OrderForm : Form
     {
+        public DatabaseConnection dbc;
+        private int idClient;
+        private int idVehicle;
+        private int phone;
         public OrderForm()
         {
             InitializeComponent();
+        }
+        public OrderForm(ref DatabaseConnection dbc_)
+        {
+            InitializeComponent();
+            this.dbc = dbc_;
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {
+            if (textboxesAreValid())
+            {
+                    dbc.query("select idClient from clients where name='" + nameTextBox.Text +
+                                                    "' and surname='" + surnameTextBox.Text +
+                                                    "' and address='" + addressTextBox.Text +
+                                                    "' and phone=" + phone.ToString() +"::NUMERIC;");
+                    if (dbc.dt.Rows.Count == 0)
+                    {
+                        dbc.query("insert into clients(name, surname, address, phone) values ('" + nameTextBox.Text + "', '" + surnameTextBox.Text + "', '" + addressTextBox.Text + "', " + phone.ToString() + "::NUMERIC);");
+                        dbc.query("select idClient from clients where name='" + nameTextBox.Text +
+                                                    "' and surname='" + surnameTextBox.Text +
+                                                    "' and address='" + addressTextBox.Text +
+                                                    "' and phone=" + phone.ToString() + "::NUMERIC;");
+                    }
+                    idClient = dbc.dt.Rows[0].Field<int>("idClient");
+                    string orderDate = DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString(); 
+                    dbc.query("insert into orders(idClient, idVehicle, orderDate) values(" + idClient.ToString() + ", " + idVehicle.ToString() + ", '" + orderDate + "');");
+                    refreshOrders();
+            }
+        }
+
+        private void refreshOrders()
+        {
+            dbc.query("select o.idorder, v.brand, v.model, v.engine, v.color from clients c join orders o using(idClient) join vehicles v using(idVehicle) where c.name='" + nameTextBox.Text +
+                                                                                                                                                          "' and c.surname='" + surnameTextBox.Text +
+                                                                                                                                                          "' and c.address='" + addressTextBox.Text +
+                                                                                                                                                          "' and c.phone=" + phone.ToString() + "::NUMERIC;");
+            ordersDataGrid.DataSource = dbc.dt.DefaultView.ToTable(false, "idorder", "brand", "model", "engine", "color");
+        }
+
+        private void returnButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void cancelOrderButton_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in ordersDataGrid.SelectedRows)
+            {
+                dbc.query("delete from orders where idOrder=" + row.Cells["idorder"].Value.ToString() + ";");
+            }
+            refreshOrders();
+        }
+
+        public void setIdVehicle(int id)
+        {
+            this.idVehicle = id;
+        }
+
+        private void showOrdersButton_Click(object sender, EventArgs e)
+        {
+            if (textboxesAreValid()) 
+            { 
+                refreshOrders();
+            }
+        }
+
+        private bool textboxesAreValid()
+        {
+            if (nameTextBox.Text == "" ||
+                surnameTextBox.Text == "" ||
+                addressTextBox.Text == "" ||
+                phoneTextBox.Text == "")
+            {
+                MessageBox.Show("Provide all necessary information!", "Information missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                phone = Int32.Parse(phoneTextBox.Text);
+                if (phone < 0 || phone > 999999999)
+                {
+                    MessageBox.Show("Provide correct phone number!", "Invalid phone number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }

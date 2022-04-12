3820fd1a9244c772cdf1ce2504d116f543396f7a
andrzejmidura <andrzej.midura253@gmail.com>
2022-04-12 18:09:25 +0200

    implemented: filling client info and orderedCarsGridView

--- CarRental/src/TestDriveReservationForm.Designer.cs
+++ CarRental/src/TestDriveReservationForm.Designer.cs
@@ -29,12 +29,200 @@ namespace CarRental
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.returnButton = new System.Windows.Forms.Button();
+            this.bookButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.orderedCarsGridView = new System.Windows.Forms.DataGridView();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.surnameLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderedCarsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.monthCalendar1.Location = new System.Drawing.Point(81, 40);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // returnButton
+            // 
+            this.returnButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.returnButton.Location = new System.Drawing.Point(218, 459);
+            this.returnButton.Name = "returnButton";
+            this.returnButton.Size = new System.Drawing.Size(180, 40);
+            this.returnButton.TabIndex = 1;
+            this.returnButton.Text = "Return to menu";
+            this.returnButton.UseVisualStyleBackColor = true;
+            // 
+            // bookButton
+            // 
+            this.bookButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bookButton.Location = new System.Drawing.Point(404, 459);
+            this.bookButton.Name = "bookButton";
+            this.bookButton.Size = new System.Drawing.Size(180, 40);
+            this.bookButton.TabIndex = 2;
+            this.bookButton.Text = "Book";
+            this.bookButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cancelButton.Location = new System.Drawing.Point(590, 459);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(180, 40);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel reservation";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(336, 246);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(558, 166);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // orderedCarsGridView
+            // 
+            this.orderedCarsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.orderedCarsGridView.Location = new System.Drawing.Point(336, 40);
+            this.orderedCarsGridView.Name = "orderedCarsGridView";
+            this.orderedCarsGridView.Size = new System.Drawing.Size(558, 162);
+            this.orderedCarsGridView.TabIndex = 5;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Enabled = false;
+            this.phoneTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.phoneTextBox.Location = new System.Drawing.Point(137, 373);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(171, 23);
+            this.phoneTextBox.TabIndex = 20;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Enabled = false;
+            this.addressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addressTextBox.Location = new System.Drawing.Point(137, 332);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(171, 23);
+            this.addressTextBox.TabIndex = 19;
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Enabled = false;
+            this.surnameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.surnameTextBox.Location = new System.Drawing.Point(137, 288);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(171, 23);
+            this.surnameTextBox.TabIndex = 18;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Enabled = false;
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameTextBox.Location = new System.Drawing.Point(137, 247);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(171, 23);
+            this.nameTextBox.TabIndex = 17;
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.phoneLabel.Location = new System.Drawing.Point(78, 373);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(53, 17);
+            this.phoneLabel.TabIndex = 16;
+            this.phoneLabel.Text = "Phone:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addressLabel.Location = new System.Drawing.Point(65, 332);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(64, 17);
+            this.addressLabel.TabIndex = 15;
+            this.addressLabel.Text = "Address:";
+            // 
+            // surnameLabel
+            // 
+            this.surnameLabel.AutoSize = true;
+            this.surnameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.surnameLabel.Location = new System.Drawing.Point(59, 290);
+            this.surnameLabel.Name = "surnameLabel";
+            this.surnameLabel.Size = new System.Drawing.Size(69, 17);
+            this.surnameLabel.TabIndex = 14;
+            this.surnameLabel.Text = "Surname:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameLabel.Location = new System.Drawing.Point(82, 249);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(49, 17);
+            this.nameLabel.TabIndex = 13;
+            this.nameLabel.Text = "Name:";
+            // 
+            // TestDriveReservationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(965, 533);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.surnameLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.orderedCarsGridView);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.bookButton);
+            this.Controls.Add(this.returnButton);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "TestDriveReservationForm";
             this.Text = "TestDriveReservation";
+            this.Load += new System.EventHandler(this.TestDriveReservationForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderedCarsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button returnButton;
+        private System.Windows.Forms.Button bookButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView orderedCarsGridView;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label surnameLabel;
+        private System.Windows.Forms.Label nameLabel;
     }
 }
--- CarRental/src/TestDriveReservationForm.cs
+++ CarRental/src/TestDriveReservationForm.cs
@@ -12,9 +12,84 @@ namespace CarRental
 {
     public partial class TestDriveReservationForm : Form
     {
+        private DatabaseConnection dbc;
+        private int idClient;
         public TestDriveReservationForm()
         {
             InitializeComponent();
+        }
+
+        public TestDriveReservationForm(ref DatabaseConnection dbc_)
+        {
+            InitializeComponent();
+            this.dbc = dbc_;
+            
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            
+        }
+
+        private void TestDriveReservationForm_Load(object sender, EventArgs e)
+        {
+            refreshClientInfo();
+        }
+
+        private void refreshOrderedCars()
+        {
+            dbc.query("select o.idOrder, v.brand, v.model, v.engine, v.color from clients c join orders o using(idClient) join vehicles v using(idVehicle) where c.idClient='" + idClient.ToString() + "';");
+            if (dbc.dt.Rows.Count == 0)
+            {
+                orderedCarsGridView.DataSource = null;
+                MessageBox.Show("No cars ordered by you.", "No cars ordered", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                try
+                {
+                    orderedCarsGridView.DataSource = dbc.dt.DefaultView.ToTable(false, "idorder", "brand", "model", "engine", "color");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Query error occured!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void refreshClientInfo()
+        {
+            if (idClient == -1)
+            {
+                MessageBox.Show("Please provide client informations in OrderForm!", "Wrong idClient", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dbc.query("select * from clients where idClient=" + idClient.ToString() + ";");
+                if (dbc.dt.Rows.Count == 1)
+                {
+                    nameTextBox.Text = dbc.dt.Rows[0].Field<string>("name");
+                    surnameTextBox.Text = dbc.dt.Rows[0].Field<string>("surname");
+                    addressTextBox.Text = dbc.dt.Rows[0].Field<string>("address");
+                    phoneTextBox.Text = dbc.dt.Rows[0].Field<Object>("phone").ToString();
+                    refreshOrderedCars();
+                }
+                else
+                {
+                    throw new Exception("Query error: Ambiguous idClient! Expected rows: 1\tgot:" + dbc.dt.Rows.Count.ToString());
+                }
+            }
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        public void setIdClient(int idClient_)
+        {
+            this.idClient = idClient_;
         }
     }
 }

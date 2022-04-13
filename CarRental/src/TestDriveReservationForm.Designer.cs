9e97638dcf930a453d67e03da75b19a21bc86b21
andrzejmidura <andrzej.midura253@gmail.com>
2022-04-13 15:44:21 +0200

    further implementation

--- CarRental/src/TestDriveReservationForm.Designer.cs
+++ CarRental/src/TestDriveReservationForm.Designer.cs
@@ -33,7 +33,7 @@ namespace CarRental
             this.returnButton = new System.Windows.Forms.Button();
             this.bookButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.reservationsDataGrid = new System.Windows.Forms.DataGridView();
             this.orderedCarsGridView = new System.Windows.Forms.DataGridView();
             this.phoneTextBox = new System.Windows.Forms.TextBox();
             this.addressTextBox = new System.Windows.Forms.TextBox();
@@ -43,63 +43,70 @@ namespace CarRental
             this.addressLabel = new System.Windows.Forms.Label();
             this.surnameLabel = new System.Windows.Forms.Label();
             this.nameLabel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.calendarLabel = new System.Windows.Forms.Label();
+            this.orderedCarsLabel = new System.Windows.Forms.Label();
+            this.reservationsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.reservationsDataGrid)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.orderedCarsGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // monthCalendar1
             // 
             this.monthCalendar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.monthCalendar1.Location = new System.Drawing.Point(81, 40);
+            this.monthCalendar1.Location = new System.Drawing.Point(81, 88);
             this.monthCalendar1.MaxSelectionCount = 1;
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
-            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
             // 
             // returnButton
             // 
             this.returnButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.returnButton.Location = new System.Drawing.Point(218, 459);
+            this.returnButton.Location = new System.Drawing.Point(208, 568);
             this.returnButton.Name = "returnButton";
             this.returnButton.Size = new System.Drawing.Size(180, 40);
             this.returnButton.TabIndex = 1;
             this.returnButton.Text = "Return to menu";
             this.returnButton.UseVisualStyleBackColor = true;
+            this.returnButton.Click += new System.EventHandler(this.returnButton_Click);
             // 
             // bookButton
             // 
             this.bookButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.bookButton.Location = new System.Drawing.Point(404, 459);
+            this.bookButton.Location = new System.Drawing.Point(394, 568);
             this.bookButton.Name = "bookButton";
             this.bookButton.Size = new System.Drawing.Size(180, 40);
             this.bookButton.TabIndex = 2;
             this.bookButton.Text = "Book";
             this.bookButton.UseVisualStyleBackColor = true;
+            this.bookButton.Click += new System.EventHandler(this.bookButton_Click);
             // 
             // cancelButton
             // 
             this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.cancelButton.Location = new System.Drawing.Point(590, 459);
+            this.cancelButton.Location = new System.Drawing.Point(580, 568);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(180, 40);
             this.cancelButton.TabIndex = 3;
             this.cancelButton.Text = "Cancel reservation";
             this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
-            // dataGridView1
+            // reservationsDataGrid
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(336, 246);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(558, 166);
-            this.dataGridView1.TabIndex = 4;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.reservationsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.reservationsDataGrid.Location = new System.Drawing.Point(336, 345);
+            this.reservationsDataGrid.Name = "reservationsDataGrid";
+            this.reservationsDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.reservationsDataGrid.Size = new System.Drawing.Size(558, 166);
+            this.reservationsDataGrid.TabIndex = 4;
             // 
             // orderedCarsGridView
             // 
             this.orderedCarsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.orderedCarsGridView.Location = new System.Drawing.Point(336, 40);
+            this.orderedCarsGridView.Location = new System.Drawing.Point(336, 88);
             this.orderedCarsGridView.Name = "orderedCarsGridView";
+            this.orderedCarsGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.orderedCarsGridView.Size = new System.Drawing.Size(558, 162);
             this.orderedCarsGridView.TabIndex = 5;
             // 
@@ -107,7 +114,7 @@ namespace CarRental
             // 
             this.phoneTextBox.Enabled = false;
             this.phoneTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.phoneTextBox.Location = new System.Drawing.Point(137, 373);
+            this.phoneTextBox.Location = new System.Drawing.Point(137, 479);
             this.phoneTextBox.Name = "phoneTextBox";
             this.phoneTextBox.Size = new System.Drawing.Size(171, 23);
             this.phoneTextBox.TabIndex = 20;
@@ -116,7 +123,7 @@ namespace CarRental
             // 
             this.addressTextBox.Enabled = false;
             this.addressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.addressTextBox.Location = new System.Drawing.Point(137, 332);
+            this.addressTextBox.Location = new System.Drawing.Point(137, 438);
             this.addressTextBox.Name = "addressTextBox";
             this.addressTextBox.Size = new System.Drawing.Size(171, 23);
             this.addressTextBox.TabIndex = 19;
@@ -125,7 +132,7 @@ namespace CarRental
             // 
             this.surnameTextBox.Enabled = false;
             this.surnameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.surnameTextBox.Location = new System.Drawing.Point(137, 288);
+            this.surnameTextBox.Location = new System.Drawing.Point(137, 394);
             this.surnameTextBox.Name = "surnameTextBox";
             this.surnameTextBox.Size = new System.Drawing.Size(171, 23);
             this.surnameTextBox.TabIndex = 18;
@@ -134,7 +141,7 @@ namespace CarRental
             // 
             this.nameTextBox.Enabled = false;
             this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.nameTextBox.Location = new System.Drawing.Point(137, 247);
+            this.nameTextBox.Location = new System.Drawing.Point(137, 353);
             this.nameTextBox.Name = "nameTextBox";
             this.nameTextBox.Size = new System.Drawing.Size(171, 23);
             this.nameTextBox.TabIndex = 17;
@@ -143,7 +150,7 @@ namespace CarRental
             // 
             this.phoneLabel.AutoSize = true;
             this.phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.phoneLabel.Location = new System.Drawing.Point(78, 373);
+            this.phoneLabel.Location = new System.Drawing.Point(78, 479);
             this.phoneLabel.Name = "phoneLabel";
             this.phoneLabel.Size = new System.Drawing.Size(53, 17);
             this.phoneLabel.TabIndex = 16;
@@ -153,7 +160,7 @@ namespace CarRental
             // 
             this.addressLabel.AutoSize = true;
             this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.addressLabel.Location = new System.Drawing.Point(65, 332);
+            this.addressLabel.Location = new System.Drawing.Point(65, 438);
             this.addressLabel.Name = "addressLabel";
             this.addressLabel.Size = new System.Drawing.Size(64, 17);
             this.addressLabel.TabIndex = 15;
@@ -163,7 +170,7 @@ namespace CarRental
             // 
             this.surnameLabel.AutoSize = true;
             this.surnameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.surnameLabel.Location = new System.Drawing.Point(59, 290);
+            this.surnameLabel.Location = new System.Drawing.Point(59, 396);
             this.surnameLabel.Name = "surnameLabel";
             this.surnameLabel.Size = new System.Drawing.Size(69, 17);
             this.surnameLabel.TabIndex = 14;
@@ -173,17 +180,50 @@ namespace CarRental
             // 
             this.nameLabel.AutoSize = true;
             this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.nameLabel.Location = new System.Drawing.Point(82, 249);
+            this.nameLabel.Location = new System.Drawing.Point(82, 355);
             this.nameLabel.Name = "nameLabel";
             this.nameLabel.Size = new System.Drawing.Size(49, 17);
             this.nameLabel.TabIndex = 13;
             this.nameLabel.Text = "Name:";
             // 
+            // calendarLabel
+            // 
+            this.calendarLabel.AutoSize = true;
+            this.calendarLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.calendarLabel.Location = new System.Drawing.Point(134, 60);
+            this.calendarLabel.Name = "calendarLabel";
+            this.calendarLabel.Size = new System.Drawing.Size(117, 20);
+            this.calendarLabel.TabIndex = 21;
+            this.calendarLabel.Text = "Choose a date:";
+            // 
+            // orderedCarsLabel
+            // 
+            this.orderedCarsLabel.AutoSize = true;
+            this.orderedCarsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.orderedCarsLabel.Location = new System.Drawing.Point(525, 60);
+            this.orderedCarsLabel.Name = "orderedCarsLabel";
+            this.orderedCarsLabel.Size = new System.Drawing.Size(154, 20);
+            this.orderedCarsLabel.TabIndex = 22;
+            this.orderedCarsLabel.Text = "Cars ordered by you:";
+            // 
+            // reservationsLabel
+            // 
+            this.reservationsLabel.AutoSize = true;
+            this.reservationsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.reservationsLabel.Location = new System.Drawing.Point(503, 319);
+            this.reservationsLabel.Name = "reservationsLabel";
+            this.reservationsLabel.Size = new System.Drawing.Size(195, 20);
+            this.reservationsLabel.TabIndex = 23;
+            this.reservationsLabel.Text = "Test drives booked by you:";
+            // 
             // TestDriveReservationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(965, 533);
+            this.ClientSize = new System.Drawing.Size(965, 645);
+            this.Controls.Add(this.reservationsLabel);
+            this.Controls.Add(this.orderedCarsLabel);
+            this.Controls.Add(this.calendarLabel);
             this.Controls.Add(this.phoneTextBox);
             this.Controls.Add(this.addressTextBox);
             this.Controls.Add(this.surnameTextBox);
@@ -193,7 +233,7 @@ namespace CarRental
             this.Controls.Add(this.surnameLabel);
             this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.orderedCarsGridView);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.reservationsDataGrid);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.bookButton);
             this.Controls.Add(this.returnButton);
@@ -201,7 +241,7 @@ namespace CarRental
             this.Name = "TestDriveReservationForm";
             this.Text = "TestDriveReservation";
             this.Load += new System.EventHandler(this.TestDriveReservationForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reservationsDataGrid)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.orderedCarsGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -214,7 +254,7 @@ namespace CarRental
         private System.Windows.Forms.Button returnButton;
         private System.Windows.Forms.Button bookButton;
         private System.Windows.Forms.Button cancelButton;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView reservationsDataGrid;
         private System.Windows.Forms.DataGridView orderedCarsGridView;
         private System.Windows.Forms.TextBox phoneTextBox;
         private System.Windows.Forms.TextBox addressTextBox;
@@ -224,5 +264,8 @@ namespace CarRental
         private System.Windows.Forms.Label addressLabel;
         private System.Windows.Forms.Label surnameLabel;
         private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label calendarLabel;
+        private System.Windows.Forms.Label orderedCarsLabel;
+        private System.Windows.Forms.Label reservationsLabel;
     }
 }
--- CarRental/src/TestDriveReservationForm.cs
+++ CarRental/src/TestDriveReservationForm.cs
@@ -14,6 +14,8 @@ namespace CarRental
     {
         private DatabaseConnection dbc;
         private int idClient;
+        private string idOrder;
+        private DateTime date;
         public TestDriveReservationForm()
         {
             InitializeComponent();
@@ -26,14 +28,10 @@ namespace CarRental
             
         }
 
-        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
-        {
-            
-        }
-
         private void TestDriveReservationForm_Load(object sender, EventArgs e)
         {
             refreshClientInfo();
+            refreshReservations();
         }
 
         private void refreshOrderedCars()
@@ -82,14 +80,101 @@ namespace CarRental
             
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         public void setIdClient(int idClient_)
         {
             this.idClient = idClient_;
+        }
+
+        private void returnButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void bookButton_Click(object sender, EventArgs e)
+        {
+            if (validateBooking())
+            {
+                string reservationDate = date.Year.ToString() + "-" + date.Month.ToString() + "-" + date.Day.ToString();
+                dbc.query("insert into reservations(idClient, idOrder, reservationDate) values("+ idClient.ToString() + ", " + idOrder + ", '" + reservationDate +"');");
+                refreshReservations();
+            }
+        }
+
+        private bool validateBooking()
+        {
+            if (date.Date.CompareTo(DateTime.Today.Date) <= 0) // its today or day from the past selected
+            {
+                MessageBox.Show("You must select future date!", "Wrong date selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else if (idClient == -1)
+            {
+                MessageBox.Show("You haven't placed any orders thus you are not registered in our database!", "Client not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else if (orderedCarsGridView.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("You must specify which car you want to test during test drive!", "Cars selection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else
+            {
+                idOrder = orderedCarsGridView.SelectedRows[0].Cells["idOrder"].Value.ToString();
+                dbc.query("select * from reservations where idOrder=" + idOrder + ";");
+                if (dbc.dt.Rows.Count != 0)
+                {
+                    MessageBox.Show("You have reserved test drive for this car already!", "Cars selection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            this.date = monthCalendar1.SelectionStart;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (reservationsDataGrid.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("No selected reservations.", "Selection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (MessageBox.Show("Are you sure you want to cancel the reservation/s?", "Cancel", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                StringBuilder rows = new StringBuilder();
+                foreach (DataGridViewRow row in reservationsDataGrid.SelectedRows)
+                {
+                    rows.Append(row.Cells["idReservation"].Value).Append(", ");
+                }
+                rows.Remove(rows.Length - 2, 2); // removes comma and space at the end
+                dbc.query("delete from reservations where idReservation in (" + rows.ToString() + ");");
+                refreshReservations();
+            }
+        }
+
+        private void refreshReservations()
+        {
+                dbc.query("select r.idReservation, r.reservationDate, r.idOrder from clients c join reservations r using(idClient) join orders o using(idOrder) where c.idClient=" + idClient.ToString() + ";");
+                if (dbc.dt.Rows.Count == 0)
+                {
+                    reservationsDataGrid.DataSource = null;
+                }
+                else
+                {
+                    try
+                    {
+                        reservationsDataGrid.DataSource = dbc.dt.DefaultView.ToTable(false, "idReservation", "reservationDate", "idOrder");
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Query error occured!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
         }
     }
 }
